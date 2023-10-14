618c8dcff4db797330bdad1cd9efaf77c317c606
Gulshan <gulshansinghhhh@gmail.com>
2023-10-14 11:43:59 +0530

    responsivenes

--- src/Components/Photos.jsx
+++ src/Components/Photos.jsx
@@ -12,20 +12,20 @@ const Photos = () => {
     <div className='my-3 relative mx-3  md:left-0 '>
       <div className=' text-white relative  lg:text-5xl md:text-4xl sm:text-3xl px-20 text-center font-semibold  text-2xl my-8'> <h1 className='heading'>Image Gallery</h1></div>
         <div className='flex justify-center gap-4 flex-col lg:flex-row '>
-            <div className='overflow:hidden mx-auto lg:mx-0  shadoww w-[22em] left-3 md:left-0 relative   md:w-[37em] rounded-2xl h-[16em] md:h-[20em] bg-white/10'>
+            <div className='overflow:hidden mx-auto lg:mx-0  shadoww w-[22em]  md:left-0 relative   md:w-[37em] rounded-2xl h-[16em] md:h-[20em] bg-white/10'>
               <img className='relative rounded-2xl w-[95%] mx-auto my-3 h-[95%]' src={unnati} alt="moments" />
             </div>
-            <div className='shadoww w-[22em] mx-auto lg:mx-0  h-[16em]  md:w-[30em] left-3 md:left-0 relative rounded-2xl md:h-[20em] bg-white/10'>
+            <div className='shadoww w-[22em] mx-auto lg:mx-0  h-[16em]  md:w-[30em]  md:left-0 relative rounded-2xl md:h-[20em] bg-white/10'>
             <img className='relative rounded-2xl w-[95%] mx-auto my-3 h-[95%]' src={unnati2} alt="moments" />
 
             </div>
         </div>
         <div className='flex py-20 justify-center gap-4 flex-col lg:flex-row '>
-            <div className='shadoww shadow-inner mx-auto lg:mx-0  w-[22em] h-[16em]  left-3 md:left-0 relative md:w-[30em] rounded-2xl md:h-[20em] bg-white/10'>
+            <div className='shadoww shadow-inner mx-auto lg:mx-0  w-[22em] h-[16em]   md:left-0 relative md:w-[30em] rounded-2xl md:h-[20em] bg-white/10'>
             <img className='relative rounded-2xl w-[95%] mx-auto my-3 h-[95%]' src={unnati3} alt="moments" />
 
             </div>
-            <div className='shadoww w-[22em] mx-auto lg:mx-0  h-[16em]  md:w-[37em] left-3 md:left-0 relative rounded-2xl md:h-[20em] bg-white/10'>
+            <div className='shadoww w-[22em] mx-auto lg:mx-0  h-[16em]  md:w-[37em]  md:left-0 relative rounded-2xl md:h-[20em] bg-white/10'>
               <img className='relative rounded-2xl w-[95%] mx-auto my-3 h-[95%]' src={unnati4} alt="moments" />
                 
             </div>
--- src/Components/Videos.jsx
+++ src/Components/Videos.jsx
@@ -9,7 +9,7 @@ const Videos = () => {
     <div className=' my-20 mx-3 relative  margin-left: 0.7em;  md:left-0'>
       <div className=' text-white lg:text-5xl md:text-4xl sm:text-3xl px-20 text-center font-semibold   text-2xl my-8'> <h1 className='heading'>Unnati Moments</h1></div>
       <div className='flex justify-center relative mx-auto gap-4 flex-col lg:flex-row '>
-        <div className='relative left-3 md:left-0 shadoww w-[22em] mx-auto lg:mx-0 md:w-[37em] rounded-2xl h-[16em] md:h-[20em] bg-white/10'>
+        <div className='relative   shadoww w-[22em] mx-auto lg:mx-0 md:w-[37em] rounded-2xl h-[16em] md:h-[20em] bg-white/10'>
           <div className='w-[95%] h-[93%] mx-auto my-3 rounded-2xl bg-transparent z-5'>
             <iframe className='h-[100%] rounded-2xl w-[100%]' src="https://www.youtube.com/embed/25GJBDBBLBM" title="UNNATI 3.0  #shorts #business #entrepreneurships #businessideas" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
           </div>
@@ -18,7 +18,7 @@ const Videos = () => {
                     <img className='relative top-[14em] bg-transparent w-[13%] ' src={playbtn} alt="" />
                 </div> */}
         </div>
-        <div className='shadoww w-[22em] mx-auto lg:mx-0 md:left-0 h-[16em] left-3 relative   md:w-[30em] rounded-2xl md:h-[20em] bg-white/10'>
+        <div className='shadoww w-[22em] mx-auto lg:mx-0  h-[16em] relative   md:w-[30em] rounded-2xl md:h-[20em] bg-white/10'>
           <div className='w-[95%] h-[93%] relative  mx-auto my-3 rounded-2xl bg-transparent z-5'>
             <iframe  className='h-[100%] rounded-2xl w-[100%]' src="https://www.youtube.com/embed/_JXinigw2mM" title="UNNATI 2.0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
           </div>
@@ -28,7 +28,7 @@ const Videos = () => {
         </div>
       </div>
       <div className='flex py-20  justify-center gap-4 flex-col lg:flex-row '>
-        <div className='shadoww mx-auto lg:mx-0 shadow-inner relative left-3 md:left-0  w-[22em] h-[16em]  md:w-[30em] rounded-2xl md:h-[20em] bg-white/10'>
+        <div className='shadoww mx-auto lg:mx-0 shadow-inner relative   w-[22em] h-[16em]  md:w-[30em] rounded-2xl md:h-[20em] bg-white/10'>
         <div className='w-[95%] h-[93%]  mx-auto my-3 rounded-2xl bg-transparent z-5'>
         <iframe  className='h-[100%] rounded-2xl w-[100%]' src="https://www.youtube.com/embed/LKNF3ANdZ08" title="Message from Alumni entrepreneur" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
           
@@ -37,7 +37,7 @@ const Videos = () => {
             <img className='relative top-[14em] bg-transparent w-[13%] ' src={playbtn} alt="" />
           </div> */}
         </div>
-        <div className='shadoww mx-auto lg:mx-0 w-[22em] relative left-3 md:left-0 h-[16em]  md:w-[37em] rounded-2xl md:h-[20em] bg-white/10'>
+        <div className='shadoww mx-auto lg:mx-0 w-[22em] relative  h-[16em]  md:w-[37em] rounded-2xl md:h-[20em] bg-white/10'>
         <div className='w-[95%] h-[93%]  mx-auto my-3 rounded-2xl bg-transparent z-5'>
         <iframe className='h-[100%] rounded-2xl w-[100%]' src="https://www.youtube.com/embed/ydTLbHizTLw" title="Cheers to Unnati 2.0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>          
           </div>
